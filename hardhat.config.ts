e9c9d56bf05919722ccb4685817061bf5b189e09
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-10-21 03:12:13 +0700

    add commands for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,7 +16,7 @@ const config: HardhatUserConfig = {
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 1,
+                runs: 1000,
             },
             viaIR: true,
             remappings: [
